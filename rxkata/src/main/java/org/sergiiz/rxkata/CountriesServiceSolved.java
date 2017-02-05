b12be9f110e867d272a9587c1e134f03f9769cf4
Qaiser Abbasi <q.abbasi1010@gmail.com>
2017-02-05 16:41:30 +0100

    CountryService finally solved ;)

--- rxkata/src/main/java/org/sergiiz/rxkata/CountriesServiceSolved.java
+++ rxkata/src/main/java/org/sergiiz/rxkata/CountriesServiceSolved.java
@@ -1,65 +1,95 @@
 package org.sergiiz.rxkata;
 
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+import io.reactivex.Single;
+import io.reactivex.functions.Predicate;
+
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.FutureTask;
 
-import io.reactivex.Observable;
-import io.reactivex.Single;
-
 class CountriesServiceSolved implements CountriesService {
+
+    private final Predicate<Country> countryPredicate = c -> c.population > 1E6;
 
     @Override
     public Single<String> countryNameInCapitals(Country country) {
-        return null; // put your solution here
+        return Single.just(country)
+                .map(c -> c.name.toUpperCase())
+                .onErrorReturnItem("[NOT AVAILABLE]");
     }
 
     public Single<Integer> countCountries(List<Country> countries) {
-        return null; // put your solution here
+        return Observable.fromIterable(countries)
+                .count()
+                .map(Long::intValue);
     }
 
     public Observable<Long> listPopulationOfEachCountry(List<Country> countries) {
-        return null; // put your solution here;
+        return Observable.fromIterable(countries)
+                .map(c -> c.population);
     }
 
     @Override
     public Observable<String> listNameOfEachCountry(List<Country> countries) {
-        return null; // put your solution here
+        return Observable.fromIterable(countries)
+                .map(c -> c.name);
     }
 
     @Override
     public Observable<Country> listOnly3rdAnd4thCountry(List<Country> countries) {
-        return null; // put your solution here
+        return Maybe.just(countries.get(3))
+                .just(countries.get(4))
+                .toObservable();
     }
 
     @Override
     public Single<Boolean> isPopulationMoreThan1Million(List<Country> countries) {
-        return null; // put your solution here
+        return Observable.fromIterable(countries)
+                .all(countryPredicate);
     }
 
 
     @Override
     public Observable<Country> listPopulationMoreThanOneMillion(List<Country> countries) {
-        return null; // put your solution here
+        return Observable.fromIterable(countries)
+                .filter(countryPredicate);
     }
 
     @Override
     public Observable<Country> listPopulationMoreThanOneMillion(FutureTask<List<Country>> countriesFromNetwork) {
-        return null; // put your solution here
+        return Observable.fromFuture(countriesFromNetwork)
+                .flatMap(this::listPopulationMoreThanOneMillion);
     }
 
     @Override
     public Observable<String> getCurrencyUsdIfNotFound(String countryName, List<Country> countries) {
-        return null; // put your solution here
+        return Observable.fromIterable(countries)
+                .filter(c -> c.name.equals(countryName))
+                .map(c -> c.currency)
+                .defaultIfEmpty("USD (default)");
     }
 
     @Override
     public Observable<Long> sumPopulationOfCountries(List<Country> countries) {
-        return null; // put your solution here
+        return listPopulationOfEachCountry(countries).reduce((c1, c2) -> c1 + c2)
+                .toObservable();
     }
 
     @Override
     public Single<Map<String, Long>> mapCountriesToNamePopulation(List<Country> countries) {
-        return null; // put your solution here
+        return Observable.zip(listNameOfEachCountry(countries), listPopulationOfEachCountry(countries), Tuple2::new)
+                .toMap(k -> k.a, v -> v.b);
+    }
+
+    private class Tuple2<A, B> {
+        final A a;
+        final B b;
+
+        Tuple2(A a, B b) {
+            this.a = a;
+            this.b = b;
+        }
     }
 }
